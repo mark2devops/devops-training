e2f77dcba4880d24c35b50c19b35197bbd079b1e
mark2devops <mark.tolentino.dev1@gmail.com>
2026-09-07 15:58:17 +0200

    Feature: Added current time to output

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main() {
-	cout << "Hello from C++ running on Linux!" << endl
+	cout << "Hello from C++ running on Linux!" << endl;
 	cout << "My user is:" << getenv("USER") << endl;
+
+	time_t now = time(0);
+	cout << "Build time : " << ctime(&now); 
+
 	return 0;
 }
